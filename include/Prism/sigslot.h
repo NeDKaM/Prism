7bcc1eab3f9eddff98e560a2cecd6ba7af6830cf
Alexandre Dubujet <alex.dubujet@gmail.com>
2016-01-31 04:06:14 +0100

    Sigslot : Format & declaration helpers

--- include/Prism/sigslot.h
+++ include/Prism/sigslot.h
@@ -3,8 +3,8 @@
  * Author : Alexandre DUBUJET
  * Creation : 2015
  * Desc. : C signal/slot implementation
- * Dependencies :	<stdarg.h>
- * Last modifications : 06/7/2015
+ * Dependencies :	<Prism/config.h>,
+                    <stdarg.h>
  */
 
 #ifndef H_PRISM_SIGSLOT_INCLUDED
@@ -25,6 +25,13 @@ extern "C"
 	extern PRISM_API void           Pr_DeleteSignal(Pr_Signal *);
 	extern PRISM_API int            Pr_Connect(Pr_Signal *, void *, Pr_Slot);
 	extern PRISM_API void           Pr_Emit(Pr_Signal *, ...);
+
+#define PR_SIGNAL(name)             Pr_Signal * name
+#define PR_SLOT(name)               void name(void *, va_list); \
+                                    void name
+#define PR_SLOT_EXTERN_API(name)    extern PRISM_API void name##_Slot(void *, va_list); \
+                                    extern PRISM_API void name
+
 
 #ifdef __cplusplus
 }
