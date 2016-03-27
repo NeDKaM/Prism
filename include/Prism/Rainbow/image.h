388d11264d37ee312951a605b3dd51e3229a4a6c
Alexandre Dubujet <alex.dubujet@gmail.com>
2016-03-27 04:04:58 +0200

    Image: Remade implementation
    + New constructor
    + New fill function

--- include/Prism/Rainbow/image.h
+++ include/Prism/Rainbow/image.h
@@ -4,18 +4,20 @@
 #include <Prism/config.h>
 #include <Prism/types.h>
 #include <Prism/Rainbow/vector.h>
+#include <Prism/Rainbow/rect.h>
 #include <Prism/Rainbow/color.h>
 
 PR_CPP_PROTECT_ON
 
     PR_STRUCT(pr_image_t, Pr_Image);
 
-    extern PRISM_API Pr_Image *         Pr_NewImage(Pr_ByteArrayRef, pr_u32_t, pr_u32_t);
+    extern PRISM_API Pr_Image *         Pr_NewImage(pr_u32_t, pr_u32_t);
     extern PRISM_API void               Pr_DeleteImage(Pr_Image *);
     extern PRISM_API pr_bool_t          Pr_GetImageSize(Pr_ImageRef, Pr_Vector2i *);
     extern PRISM_API void               Pr_SetImagePixel(Pr_Image *, Pr_ColorRef, pr_u32_t, pr_u32_t);
     extern PRISM_API pr_bool_t          Pr_GetImagePixel(Pr_ImageRef, Pr_Color *, pr_u32_t, pr_u32_t);
     extern PRISM_API Pr_ByteArrayRef    Pr_GetImagePixels(Pr_ImageRef);
+    extern PRISM_API void               Pr_FillImageRect(Pr_Image *, Pr_RectRef(long), Pr_ColorRef);
 
 PR_CPP_PROTECT_OFF
 
--- src/Prism/Rainbow/image.c
+++ src/Prism/Rainbow/image.c
@@ -5,16 +5,15 @@
 #include <string.h>
 
 struct pr_image_t {
-    Pr_Array(pr_u8_t)   pixels;
-    Pr_Vector2i         size;
+    pr_u8_t *   pixels;
+    Pr_Vector2i size;
 };
 
-Pr_Image * Pr_NewImage(Pr_ByteArrayRef ap_bytes, pr_u32_t a_w, pr_u32_t a_h)
+Pr_Image * Pr_NewImage(pr_u32_t a_w, pr_u32_t a_h)
 {
     Pr_Image * lp_out;
     pr_u32_t l_size;
 
-    if (!ap_bytes) return NULL;
     if (a_w == 0 || a_h == 0) return NULL;
 
     lp_out = malloc(sizeof(Pr_Image));
@@ -22,13 +21,14 @@ Pr_Image * Pr_NewImage(Pr_ByteArrayRef ap_bytes, pr_u32_t a_w, pr_u32_t a_h)
 
     l_size = a_w * a_h * 4;
 
-    Pr_MakeSizedArray(lp_out->pixels, l_size);
-    if (Pr_ArraySize(lp_out->pixels) == 0) {
+    lp_out->pixels = calloc(l_size, sizeof(pr_u8_t));
+    if (!lp_out->pixels) {
         free(lp_out);
         return NULL;
     }
 
-    memcpy(lp_out->pixels.list, ap_bytes, l_size);
+    lp_out->size.x = a_w;
+    lp_out->size.y = a_h;
 
     return lp_out;
 }
@@ -37,7 +37,7 @@ void Pr_DeleteImage(Pr_Image * ap_img)
 {
     if (!ap_img) return;
 
-    Pr_ClearArray(ap_img->pixels);
+    free(ap_img->pixels);
     free(ap_img);
 }
 
@@ -54,7 +54,7 @@ Pr_ByteArrayRef Pr_GetImagePixels(Pr_ImageRef ap_img)
 {
     if (!ap_img) return NULL;
 
-    return ap_img->pixels.list;
+    return ap_img->pixels;
 }
 
 void Pr_SetImagePixel(Pr_Image * ap_img, Pr_ColorRef ap_color, pr_u32_t a_x, pr_u32_t a_y)
@@ -64,7 +64,7 @@ void Pr_SetImagePixel(Pr_Image * ap_img, Pr_ColorRef ap_color, pr_u32_t a_x, pr_
     if (!ap_img) return;
     if (a_x >= (pr_u32_t)ap_img->size.x || a_y >= (pr_u32_t)ap_img->size.y) return;
 
-    lp_pixels = &Pr_ArrayAt(ap_img->pixels, (a_x + a_y * ap_img->size.x) * 4);
+    lp_pixels = ap_img->pixels + (a_x + a_y * ap_img->size.x) * 4;
 
     lp_pixels[0] = ap_color->r;
     lp_pixels[1] = ap_color->g;
@@ -79,7 +79,7 @@ pr_bool_t Pr_GetImagePixel(Pr_ImageRef ap_img, Pr_Color * ap_color, pr_u32_t a_x
     if (!ap_img || !ap_color) return PR_FALSE;
     if (a_x >= (pr_u32_t)ap_img->size.x || a_y >= (pr_u32_t)ap_img->size.y) return PR_FALSE;
 
-    lp_pixels = &Pr_ArrayAt(ap_img->pixels, (a_x + a_y * ap_img->size.x) * 4);
+    lp_pixels = ap_img->pixels + (a_x + a_y * ap_img->size.x) * 4;
     
     ap_color->r = lp_pixels[0];
     ap_color->b = lp_pixels[1];
@@ -87,5 +87,48 @@ pr_bool_t Pr_GetImagePixel(Pr_ImageRef ap_img, Pr_Color * ap_color, pr_u32_t a_x
     ap_color->a = lp_pixels[3];
 
     return PR_TRUE;
+}
+
+void Pr_FillImageRect(Pr_Image * ap_img, Pr_RectRef(long) ap_rect, Pr_ColorRef ap_color)
+{
+    Pr_Recti    l_rect;
+    Pr_Color    l_color;
+    pr_u8_t *   lp_pixels;
+    pr_u32_t    l_w;
+
+    if (!ap_img) return;
+
+    if (ap_rect) {
+        l_rect.x        = (ap_rect->x < 0) ? 0 : ap_rect->x;
+        l_rect.y        = (ap_rect->y < 0) ? 0 : ap_rect->y;
+        l_rect.width    = (ap_rect->width > ap_img->size.x) ? ap_img->size.x : ap_rect->width;
+        l_rect.height   = (ap_rect->height > ap_img->size.y) ? ap_img->size.y : ap_rect->height;
+    } else {
+        l_rect.x        = 0;
+        l_rect.y        = 0;
+        l_rect.width    = ap_img->size.x;
+        l_rect.height   = ap_img->size.y;
+    }
+
+    if (ap_color) {
+        memcpy(&l_color, ap_color, sizeof(Pr_Color));
+    } else {
+        memset(ap_img->pixels, 0, ap_img->size.x * ap_img->size.y * 4);
+        return;
+    }
+
+    lp_pixels = ap_img->pixels + (l_rect.x + l_rect.y * ap_img->size.x) * 4;
+
+    while (l_rect.height--) {
+        l_w = l_rect.width;
+        while (l_w--) {
+            *lp_pixels++ = l_color.r;
+            *lp_pixels++ = l_color.g;
+            *lp_pixels++ = l_color.b;
+            *lp_pixels++ = l_color.a;
+        }
+
+        lp_pixels += (ap_img->size.x - (l_rect.x + l_rect.width)) + l_rect.x;
+    }
 }
 
