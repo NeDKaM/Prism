f845a53139d18f858601db954a0b2f4ba2f968f0
Alexandre Dubujet <alex.dubujet@gmail.com>
2016-08-07 16:53:40 +0200

    Removed GUI system includes

--- include/Prism/prism.h
+++ include/Prism/prism.h
@@ -3,16 +3,12 @@
 
 #include <Prism/application.h>
 #include <Prism/array.h>
-#include <Prism/class.h>
 #include <Prism/config.h>
-#include <Prism/ecs.h>
-#include <Prism/library.h>
 #include <Prism/list.h>
 #include <Prism/logger.h>
 #include <Prism/sigslot.h>
 #include <Prism/string.h>
 #include <Prism/types.h>
-#include <Prism/var.h>
 #include <Prism/window.h>
 
 #endif /* H_PRISM_INCLUDED */
