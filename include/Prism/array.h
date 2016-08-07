9bb181c724d903cb6d6b15fca6c0f8d8b74fe810
Alexandre Dubujet <alex.dubujet@gmail.com>
2016-08-07 16:40:38 +0200

    Remade Pr_Array
    Pr_Array is now a fully dedicated object

--- include/Prism/array.h
+++ include/Prism/array.h
@@ -9,137 +9,22 @@
 #ifndef H_PRISM_ARRAY_INCLUDED
 #define H_PRISM_ARRAY_INCLUDED
 
-#include <stdlib.h>
+#include <Prism/config.h>
+#include <Prism/types.h>
 
 PR_CPP_PROTECT_ON
+
+    PR_STRUCT(pr_array_t, Pr_Array);
     
-    /* Utility functions ;) */
-    #define Pr_ArrayAt(a_array,a_at)  (a_array).list[(a_at)]
-    #define Pr_ArraySize(a_array)     (a_array).size
-    #define Pr_ArrayCapacity(a_array) (a_array).capacity
-
-    #define Pr_Array(a_Type) \
-        struct { \
-            a_Type * list; \
-            unsigned long size; \
-            unsigned long capacity; \
-            int reducible; \
-        } 
-
-    #define Pr_SetArrayDynamic(a_array,a_bool) \
-        do { \
-            a_array.reducible = a_bool; \
-        } while (0)
-
-    #define Pr_MakeArray(a_array) \
-        do { \
-            a_array.size = 0; \
-            a_array.capacity = 0; \
-            a_array.list = NULL; \
-            a_array.reducible = 0; \
-        } while (0)
-
-    #define Pr_MakeSizedArray(a_array,a_size) \
-        do { \
-            unsigned long l_cap = 1; \
-            \
-            if (a_size < 1) { \
-                Pr_MakeArray(a_array); \
-            break; \
-            } \
-            \
-            while (l_cap < a_size) { \
-                l_cap *= 2; \
-            } \
-            \
-            a_array.list = malloc(l_cap*sizeof(*a_array.list)); \
-            if (a_array.list) { \
-                a_array.size = a_size; \
-                a_array.capacity = l_cap; \
-                a_array.reducible = 0; \
-            } else { \
-                Pr_MakeArray(a_array); \
-            } \
-        } while (0) 
-
-    #define Pr_ClearArray(a_array) \
-        do { \
-            a_array.size = 0; \
-            a_array.capacity = 0; \
-            free(a_array.list); \
-            a_array.list = NULL; \
-        } while (0)
-
-    #define Pr_PushBackArray(a_array) \
-        do { \
-            if (!a_array.size) { \
-                a_array.list = malloc(sizeof(*a_array.list)); \
-                if (a_array.list) { \
-                    a_array.size++; \
-                    a_array.capacity = 1; \
-                } \
-            } else { \
-                if (a_array.size == a_array.capacity) { \
-                    void * lp_tmp = realloc(a_array.list,sizeof(*a_array.list) * a_array.capacity * 2); \
-                    if (lp_tmp) { \
-	                    a_array.list = lp_tmp; \
-	                    a_array.size++; \
-	                    a_array.capacity *= 2; \
-                    } \
-                } else { \
-                    a_array.size++; \
-                } \
-            } \
-        } while (0)
-
-    #define Pr_PopBackArray(a_array) \
-        do { \
-            if (a_array.size) { \
-                a_array.size--; \
-                if (a_array.reducible) { \
-                    unsigned long l_cap = a_array.capacity; \
-                    while (a_array.size <= (l_cap >> 1)) { \
-	                    l_cap >>= 1; \
-                    } \
-                    \
-                    if (a_array.capacity) { \
-	                    void * lp_tmp = realloc(a_array.list,l_cap); \
-	                    if (lp_tmp) { \
-		                    a_array.list = lp_tmp; \
-		                    a_array.capacity = l_cap; \
-	                    } \
-                    } else { \
-	                    free(a_array.list); \
-	                    a_array.list = NULL; \
-                    } \
-                } \
-            } \
-        } while (0)
-
-    #define Pr_ExtendArray(a_array,a_size) \
-        do { \
-            if (!a_array.size) { \
-                Pr_MakeSizedArray(a_array,a_size); \
-            } else { \
-                if (a_size > 0) { \
-                    unsigned long l_size = a_size + a_array.size; \
-                    unsigned long l_cap = a_array.capacity; \
-                    void * lp_tmp; \
-                    \
-                    while (l_cap < l_size) { \
-	                    l_cap *= 2; \
-                    } \
-                    \
-                    lp_tmp = realloc(a_array.list, l_cap*sizeof(*a_array.list)); \
-                    if (lp_tmp) { \
-	                    a_array.list = lp_tmp; \
-	                    a_array.size = l_size; \
-	                    a_array.capacity = l_cap; \
-                    } \
-                } \
-            } \
-        } while (0)
+    Pr_Array *      Pr_NewArray(pr_u32_t, pr_u32_t);
+    void            Pr_DeleteArray(Pr_Array *);
+    pr_u32_t        Pr_ArraySize(Pr_ArrayRef);
+    pr_u32_t        Pr_ArrayCapacity(Pr_ArrayRef);
+    void * const    Pr_GetArrayPointer(Pr_ArrayRef);
+    pr_bool_t       Pr_ResizeArray(Pr_Array *, pr_u32_t);
+    void            Pr_ClearArray(Pr_Array *);
 
 PR_CPP_PROTECT_OFF
 
 #endif
+
---
+++ src/Prism/array.c
@@ -0,0 +1,104 @@
+#include <Prism/array.h>
+#include <stdlib.h>
+
+struct pr_array_t {
+    void *      ptr;
+    pr_u32_t    size;
+    pr_u32_t    capacity;
+    pr_u32_t    chunk; 
+};
+
+static pr_bool_t s_Pr_ExtendArray(Pr_Array * ap_array, pr_u32_t a_size)
+{
+    void *      lp_tmp;
+    pr_u32_t    l_cap;
+
+    l_cap = (ap_array->capacity > 0) ? ap_array->capacity : 1;
+
+    while (l_cap <= a_size) {
+        l_cap <<= 1;
+    }
+
+    if (!ap_array->ptr) {
+        lp_tmp = malloc(l_cap * ap_array->chunk);
+    } else {
+        lp_tmp = realloc(ap_array->ptr, l_cap * ap_array->chunk);
+    }
+
+    if (!lp_tmp) return PR_FALSE;
+
+    ap_array->ptr       = lp_tmp;
+    ap_array->capacity  = l_cap;
+    ap_array->size      = a_size;
+
+    return PR_TRUE;
+}
+
+Pr_Array * Pr_NewArray(pr_u32_t a_size, pr_u32_t a_chunk)
+{
+    Pr_Array * lp_out;
+
+    if (a_chunk == 0) return 0;
+
+    lp_out = malloc(sizeof(Pr_Array));
+    if (!lp_out) return NULL;
+
+    lp_out->chunk       = a_chunk;
+    lp_out->size        = 0;
+    lp_out->capacity    = 0;
+    lp_out->ptr         = NULL;
+
+    if (!s_Pr_ExtendArray(lp_out, a_size)) {
+        free(lp_out);
+        return NULL;
+    }
+
+    return lp_out;
+}
+
+void Pr_DeleteArray(Pr_Array * ap_array)
+{
+    if (!ap_array) return;
+
+    if (ap_array->ptr) {
+        free(ap_array->ptr);
+    }
+
+    free(ap_array);
+}
+
+pr_u32_t Pr_ArraySize(Pr_ArrayRef ap_array)
+{
+    return (ap_array) ? ap_array->size : 0;
+}
+
+pr_u32_t Pr_ArrayCapacity(Pr_ArrayRef ap_array)
+{
+    return (ap_array) ? ap_array->capacity : 0;
+}
+
+void * const Pr_GetArrayPointer(Pr_ArrayRef ap_array)
+{
+    return (ap_array) ? ap_array->ptr : NULL;
+}
+
+pr_bool_t Pr_ResizeArray(Pr_Array * ap_array, pr_u32_t a_size)
+{
+    if (!ap_array) return PR_FALSE;
+
+    if (a_size < ap_array->capacity) {
+        ap_array->size = a_size;
+        return PR_TRUE;
+    } 
+
+    return s_Pr_ExtendArray(ap_array, a_size);
+}
+
+void Pr_ClearArray(Pr_Array * ap_array)
+{
+    if (!ap_array) return;
+
+    ap_array->size = 0;
+}
+
+
