271082cca092020276742d2e9ba6bcdb773b3ddb
Alexandre Dubujet <alex.dubujet@gmail.com>
2016-08-07 17:04:25 +0200

    Vertex : fixed structure order for OGL calls

--- include/Prism/Rainbow/vertex.h
+++ include/Prism/Rainbow/vertex.h
@@ -9,8 +9,8 @@ PR_CPP_PROTECT_ON
 
     PR_STRUCT(pr_vertex_t, Pr_Vertex) {
         Pr_Vector2f position;
-        Pr_Vector2f texCoords;
         Pr_Color    color;
+        Pr_Vector2f texCoords;
     };
 
 PR_CPP_PROTECT_OFF
