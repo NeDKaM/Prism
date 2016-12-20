4fc766c8ffd2f0ea6deae590ca00f8e65aae530b
Alexandre Dubujet <alex.dubujet@gmail.com>
2016-12-20 20:34:06 +0100

    Introducing Objects & Class
    - Integrated memory pool management (user choice)
    - Inheritance (slots & properties)
    - Pr_New & Pr_Delete : universal object instanciation functions
    - Ref suffix refers now to an object pointer
    - Class are static and user defined from an universal class structure
    - Slots are now part of the object

---
+++ include/Prism/objects.h
@@ -0,0 +1,48 @@
+#ifndef H_PRISM_OBJECTS_INCLUDED
+#define H_PRISM_OBJECTS_INCLUDED
+
+#include <Prism/types.h>
+#include <Prism/memory.h>
+#include <Prism/sigslot.h>
+
+PR_CPP_PROTECT_ON
+
+    #define PR_OBJECT(name_t, name) \
+        typedef struct name_t name; \
+        typedef name * name##Ref; \
+        struct name_t 
+
+    #define Pr_SlotFunction(name) \
+        Pr_Slot slot_##name; \
+        void (*name)
+
+    #define PR_INHERIT(type) type self
+
+    typedef struct pr_class_t Pr_Class;
+   
+    PR_OBJECT(pr_object_t, Pr_Object) {
+        Pr_Class *  class;
+        Pr_Signal * onDelete;
+    };
+
+    struct pr_class_t {
+        char const *    name;
+        pr_u32_t        objectSize;
+        pr_u32_t        refCount;
+        Pr_Class *      inherit;
+        Pr_MemoryPool * memoryPool;
+        pr_u8_t         poolSize;
+        pr_bool_t       abstract;
+
+        pr_bool_t   (*construct)(Pr_ObjectRef);
+        void        (*destruct)(Pr_ObjectRef);
+    };
+
+    extern PRISM_API Pr_Class Pr_ObjectClass;
+
+    extern PRISM_API void * Pr_New(Pr_Class *);
+    extern PRISM_API void   Pr_Delete(Pr_ObjectRef);
+
+PR_CPP_PROTECT_OFF
+
+#endif /* H_PRISM_OBJECTS_INCLUDED */
---
+++ src/Prism/objects.c
@@ -0,0 +1,158 @@
+#include <Prism/objects.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+Pr_Class Pr_ObjectClass = {
+    /* name */          "Pr_ObjectClass",
+    /* objectSize */    sizeof(Pr_Object),
+    /* refCount */      0,
+    /* inherit */       NULL,
+    /* memoryPool */    NULL,
+    /* poolSize */      0,
+    /* abstract */      PR_TRUE,
+
+    NULL,
+    NULL
+};
+
+static void s_Pr_UpdateClassRefCounters(Pr_Class * ap_cls, long a_value)
+{
+    Pr_Class * lp_tmp = ap_cls;
+        
+    while (lp_tmp) {
+        lp_tmp->refCount += a_value;
+        lp_tmp = lp_tmp->inherit;
+    }    
+}
+
+static pr_bool_t s_Pr_InheritedConstructs(Pr_ObjectRef ap_obj)
+{
+    Pr_Class * lp_cls = ap_obj->class;
+    pr_u32_t l_err = 0;
+    pr_u32_t l_counter = 0;
+
+    while (lp_cls->inherit) {
+        lp_cls = lp_cls->inherit;
+        l_counter++;
+    }
+
+    lp_cls = ap_obj->class;
+
+    while (l_counter) {
+        pr_u32_t l_i;
+        for (l_i = 0 ; l_i < l_counter ; l_i++) {
+            lp_cls = lp_cls->inherit;
+        }
+
+        if (lp_cls->construct) {
+            if (!lp_cls->construct(ap_obj)) {
+                l_err++;
+                break;
+            }
+        }
+
+        l_counter--;
+        lp_cls = ap_obj->class;
+    }
+
+    if (lp_cls->construct) {
+        if (!lp_cls->construct(ap_obj)) {
+            l_err++;
+        }
+    }
+
+    return (l_err) ? PR_FALSE : PR_TRUE; 
+}
+
+static void s_Pr_InheritedDestructs(Pr_ObjectRef ap_obj)
+{		
+    Pr_Class * lp_cls = ap_obj->class;
+
+    if (lp_cls->destruct) {
+        lp_cls->destruct(ap_obj);
+    }
+
+    while (lp_cls->inherit) {
+        lp_cls = lp_cls->inherit;
+        if (lp_cls->destruct) {
+            lp_cls->destruct(ap_obj);
+        }
+    }
+}
+
+void * Pr_New(Pr_Class * ap_cls)
+{
+    Pr_ObjectRef lp_out = NULL;
+
+    if (!ap_cls) return NULL;
+    
+    if (ap_cls->abstract) return NULL;
+
+    if (!ap_cls->memoryPool) {
+        ap_cls->poolSize = (ap_cls->poolSize) ? ap_cls->poolSize : 16;
+        ap_cls->memoryPool = Pr_NewMemoryPool(
+            NULL, 
+            ap_cls->objectSize, 
+            ap_cls->poolSize
+        );
+
+        if (!ap_cls->memoryPool) return NULL;
+
+#ifdef PRISM_DEBUG
+        printf("Allocating new memory pool for %d %s objects of %d bytes each\n",
+            ap_cls->poolSize,
+            ap_cls->name,
+            ap_cls->objectSize
+        );
+#endif
+    }
+
+    lp_out = Pr_AllocateBlock(ap_cls->memoryPool);
+    if (!lp_out) return NULL;
+
+    lp_out->class = ap_cls;
+    lp_out->onDelete = Pr_NewSignal();
+
+    if (lp_out->onDelete) {
+        if (s_Pr_InheritedConstructs(lp_out)) {
+            s_Pr_UpdateClassRefCounters(ap_cls, 1);
+            return lp_out;
+        }
+
+        Pr_DeleteSignal(lp_out->onDelete);
+    }
+
+    Pr_FreeBlock(ap_cls->memoryPool, lp_out);
+
+    return NULL;
+}
+
+void Pr_Delete(Pr_ObjectRef ap_obj)
+{
+    Pr_Class * lp_cls = NULL;
+
+    if (!ap_obj) return;
+
+    lp_cls = ap_obj->class;
+
+    Pr_Emit(ap_obj->onDelete);
+
+    s_Pr_InheritedDestructs(ap_obj);
+
+    Pr_FreeBlock(lp_cls->memoryPool, ap_obj);
+
+    s_Pr_UpdateClassRefCounters(lp_cls, -1);
+
+    if (lp_cls->refCount == 0) {
+#ifdef PRISM_DEBUG
+        printf("Removing memory pool of %d %s objects of %d bytes each\n",
+            lp_cls->poolSize,
+            lp_cls->name,
+            lp_cls->objectSize
+        );
+#endif
+        Pr_DeleteMemoryPool(lp_cls->memoryPool);
+        lp_cls->memoryPool = NULL;
+    }
+}
+
