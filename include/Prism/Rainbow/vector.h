b7c185a5c64160a6d3131809de9b2615f9e8ce7d
Alexandre Dubujet <alex.dubujet@gmail.com>
2016-12-20 20:35:51 +0100

    Vector : Back to simpler implementation

--- include/Prism/Rainbow/vector.h
+++ include/Prism/Rainbow/vector.h
@@ -5,20 +5,15 @@
 
 PR_CPP_PROTECT_ON
 
-    #define Pr_Vector2(type)    Pr_Vector2##type
-    #define Pr_Vector2Ref(type) Pr_Vector2##type##Ref
+    PR_STRUCT(pr_verctor2i_t, Pr_Vector2i) {
+        pr_u32_t x;
+        pr_u32_t y;
+    };
 
-    #define PR_VECTOR2(type) \
-        typedef struct pr_vector2##type##_t Pr_Vector2##type; \
-        typedef const Pr_Vector2##type *    Pr_Vector2##type##Ref; \
-        struct pr_vector2##type##_t { \
-            type x; \
-            type y; \
-        }
-
-    PR_VECTOR2(long);   typedef Pr_Vector2(long)    Pr_Vector2i;
-    PR_VECTOR2(float);  typedef Pr_Vector2(float)   Pr_Vector2f;
-    PR_VECTOR2(double); typedef Pr_Vector2(double)  Pr_Vector2d;
+    PR_STRUCT(pr_vector2f_t, Pr_Vector2f) {
+        float x;
+        float y;
+    };
 
 PR_CPP_PROTECT_OFF
 
