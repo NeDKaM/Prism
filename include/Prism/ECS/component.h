f8831de28e37c48254b67b1425f27f2955614703
Alexandre Dubujet <alex.dubujet@gmail.com>
2017-07-23 01:31:09 +0200

    ECS : missing component.h
    oops

---
+++ include/Prism/ECS/component.h
@@ -0,0 +1,26 @@
+#ifndef H_ECS_COMPONENT_INCLUDED
+#define H_ECS_COMPONENT_INCLUDED
+
+#include <Prism/config.h>
+
+#include <Prism/memory.h>
+
+PR_CPP_PROTECT_ON
+
+    typedef struct pr_componentinfo_t Pr_ComponentInfo;
+    struct pr_componentinfo_t {
+        pr_u32_t        id;
+        Pr_Initializer  initializer;
+        pr_u32_t        dataSize;
+    };
+
+    PR_STRUCT(pr_componenthandler_t, Pr_ComponentHandler) {
+        pr_u32_t    id;
+        void *      data;
+    };
+
+PR_CPP_PROTECT_OFF
+
+#endif /* H_ECS_COMPONENET_INCLUDED */
+
+
