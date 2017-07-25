10b6b4fb869307477e36ef859598c4ad1c1a166f
Alexandre Dubujet <alex.dubujet@gmail.com>
2017-07-25 19:51:32 +0200

    ECS : Added render component
    Consists of an array of renderable objects (Pr_Renderable)

---
+++ include/Prism/RainbowSDL/rendercomponent.h
@@ -0,0 +1,29 @@
+#ifndef H_RAINBOWSDL_RENDERCOMPONENT_INCLUDED
+#define H_RAINBOWSDL_RENDERCOMPONENT_INCLUDED
+
+#include <Prism/config.h>
+
+#include <Prism/ECS/component.h>
+
+#include <Prism/memory.h>
+
+PR_CPP_PROTECT_ON
+
+    #define PR_RENDERCOMPONENT /**/ 1 /**/
+
+    extern PRISM_API Pr_ComponentInfo Pr_RenderComponentInfo;
+
+    PR_STRUCT(pr_rendercomponent_t, Pr_RenderComponent) {
+        Pr_Array *  renderables;
+    };
+
+    extern PRISM_API pr_bool_t  Pr_AttachRenderable(Pr_RenderComponent *, Pr_Renderable *);
+
+PR_CPP_PROTECT_OFF
+
+#endif
+
+/*
+
+
+*/
---
+++ src/Prism/RainbowSDL/rendercomponent.c
@@ -0,0 +1,51 @@
+#include <Prism/RainbowSDL/rendercomponent.h>
+#include <Prism/RainbowSDL/renderable.h>
+
+#include <Prism/types.h>
+
+static pr_bool_t s_Pr_RenderableInitializer(void * ap_data, pr_u32_t a_size)
+{
+    Pr_Renderable * lp_renderable = ap_data;
+    SDL_Rect l_zero = { 0, 0, 0, 0 };
+
+    lp_renderable->texture          = NULL;
+    lp_renderable->textureCoords    = l_zero;
+
+    lp_renderable->origin.x = 0;
+    lp_renderable->origin.y = 0;
+
+    return PR_TRUE;
+}
+
+static pr_bool_t s_Pr_ComponentInitializer(void * ap_data, pr_u32_t a_size)
+{
+    Pr_RenderComponent * lp_component = ap_data;
+
+    lp_component->renderables = Pr_NewArray(sizeof(Pr_RenderComponent), s_Pr_RenderableInitializer);
+    if (!lp_component->renderables) return PR_FALSE;
+
+    return PR_TRUE;
+}
+
+static void s_Pr_ComponentDeleter(void * ap_data)
+{
+    Pr_RenderComponent * lp_component = ap_data;
+
+    if (!lp_component) return;
+
+    Pr_DeleteArray(lp_component->renderables);
+}
+
+Pr_ComponentInfo Pr_RenderComponentInfo = {
+    PR_RENDERCOMPONENT,
+    s_Pr_ComponentInitializer,
+    s_Pr_ComponentDeleter,
+    sizeof(Pr_RenderComponent)
+};
+
+pr_bool_t  Pr_AttachRenderable(Pr_RenderComponent * ap_comp, Pr_Renderable * ap_rnd)
+{
+    if (!ap_comp || ap_rnd) return PR_FALSE;
+
+    return Pr_SetArrayAt(ap_comp->renderables, Pr_ArraySize(ap_comp->renderables), ap_rnd);
+}
